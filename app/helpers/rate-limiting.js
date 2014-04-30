7203dc48354094748a4c5921c3dce7e87c0887ef
Louis Simons <lousimons@gmail.com>
2014-04-29 20:20:28 -0400

    Comment out nonworking code

--- app/helpers/rate-limiting.js
+++ app/helpers/rate-limiting.js
@@ -1,14 +1,14 @@
 // From http://alexspeller.com/2013/08/09/Debounced_and_Throttled_Observers_in_Ember.html
 // todo Fix this
 
-Ember.debouncedObserver = function(keys..., time, func) {
-  Em.observer(function() {
-    Em.run.debounce(this, func, time, keys...);
-  });
-}
+// Ember.debouncedObserver = function(keys..., time, func) {
+//   Em.observer(function() {
+//     Em.run.debounce(this, func, time, keys...);
+//   });
+// }
 
-Ember.throttledObserver = function(keys..., time, func) {
-  Em.observer(function() {
-    Em.run.throttle(this, func, time, keys...);
-  });
-}
+// Ember.throttledObserver = function(keys..., time, func) {
+//   Em.observer(function() {
+//     Em.run.throttle(this, func, time, keys...);
+//   });
+// }
